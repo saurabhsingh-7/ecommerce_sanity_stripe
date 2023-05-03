e2d298e2fb605173523fddd7bf378a0337779347
Saurabh SIngh <saurabhsinghup@gmail.com>
2023-05-04 00:56:12 +0530

    sanity project

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>RANJIT Mastery Store</title>
+        <title>Saurabh Mastery Store</title>
       </Head>
       <header>
         <Navbar />
